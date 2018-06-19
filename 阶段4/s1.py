93421430ff9e1f10d21a5f7259f6cba7a6ae9aa1
zengxiaoyan <1264983873@qq.com>
2018-06-19 16:29:07 +0800

    README

--- 阶段4/s1.py
+++ 阶段4/s1.py
@@ -5,6 +5,7 @@
 import re
 import copy
 class TextInput(object):
+    """生成input标签"""
     def __init__(self,attrs=None):
         self.attrs=attrs
         if attrs:
@@ -21,6 +22,7 @@ class TextInput(object):
         return tpl
 
 class EmailInput(object):
+    """接收键值对并赋值到标签上面"""
     def __init__(self, attrs=None):
         self.attrs = attrs
         if attrs:
@@ -45,12 +47,15 @@ class Field(object):
         return str(self.widget)
 
 class CharField(Field):
+    #默认字段生成什么插件
     default_widget = TextInput
+    #匹配的正则表达式
     regex="\w+"
     def __init__(self,widget=None):
         self.value = None
         self.widget=widget if widget else self.default_widget()
 
+    #匹配成功则返回True
     def valid_field(self,value):
         self.value = value
         if re.match(self.regex,value):
@@ -80,11 +85,14 @@ class BaseForm(object):
         self.fields={}
 
         for name,field in type(self).__dict__.items():
+            #判断生成的字段是否是来自一个基类Field
             if isinstance(field,Field):
+                #避免使用的是同一个实例
                 new_field=copy.deepcopy(field)
                 setattr(self,name,new_field)
                 self.fields[name]=new_field
 
+    #输入的值与正则表达式匹配成功则返回true
     def is_valid(self):
         flag = True
         for name,field in self.fields.items():
@@ -94,7 +102,7 @@ class BaseForm(object):
                 flag=False
             return flag
 
-#使用
+#form组件是根据使用的角度定制的
 class LoginForm(BaseForm):
     user=CharField()
     email=EmailField()
